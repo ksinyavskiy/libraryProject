914f42ebb80301a778acbc4d48c42011380d225b
ksinyavskiy <kirill.sinyavskiy@cengage.com>
2021-09-16 19:00:43 +0300

    Changes:  - added configuration class-stub;  - setup connection to in-memory db.

---
+++ src/main/java/com/nix/lpr/library/config/SpringConfig.java
@@ -0,0 +1,9 @@
+package com.nix.lpr.library.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"com.nix.lpr.library"})
+public class SpringConfig {
+}
