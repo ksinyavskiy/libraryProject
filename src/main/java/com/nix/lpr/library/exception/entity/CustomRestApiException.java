095e39b828feff584d4870ef99979d09229f8cdd
Sinyavskiy Kirill <javadevsinyavskiy@gmail.com>
2021-11-01 23:31:17 +0200

    - renamed basic classes of the global exception handling.

--- src/main/java/com/nix/lpr/library/exception/entity/CustomRestApiException.java
+++ src/main/java/com/nix/lpr/library/exception/entity/CustomRestApiException.java
@@ -4,12 +4,12 @@ import org.springframework.http.HttpStatus;
 
 import java.time.ZonedDateTime;
 
-public class RestApiException {
+public class CustomRestApiException {
     private final String message;
     private final HttpStatus httpStatus;
     private final ZonedDateTime zonedDateTime;
 
-    public RestApiException(String message, HttpStatus httpStatus, ZonedDateTime zonedDateTime) {
+    public CustomRestApiException(String message, HttpStatus httpStatus, ZonedDateTime zonedDateTime) {
         this.message = message;
         this.httpStatus = httpStatus;
         this.zonedDateTime = zonedDateTime;
--- src/main/java/com/nix/lpr/library/exception/handler/CustomGlobalExceptionHandler.java
+++ src/main/java/com/nix/lpr/library/exception/handler/CustomGlobalExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.nix.lpr.library.exception.handler;
 
-import com.nix.lpr.library.exception.entity.RestApiException;
+import com.nix.lpr.library.exception.entity.CustomRestApiException;
 import com.nix.lpr.library.exception.entity.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,8 +14,8 @@ public class CustomGlobalExceptionHandler {
 
     @ExceptionHandler(value = {UserNotFoundException.class})
     public ResponseEntity<Object> handleRestApiException(Exception e) {
-        RestApiException restApiException =
-                new RestApiException(e.getMessage(), HttpStatus.NOT_FOUND, ZonedDateTime.now());
+        CustomRestApiException restApiException =
+                new CustomRestApiException(e.getMessage(), HttpStatus.NOT_FOUND, ZonedDateTime.now());
         return new ResponseEntity<>(restApiException, HttpStatus.NOT_FOUND);
     }
 }
