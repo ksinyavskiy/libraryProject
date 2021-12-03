59c4a3361a641ec687d97ab968ed6188765fc6cb
ksinyavskiy <kirill.sinyavskiy@cengage.com>
2021-12-03 13:06:39 +0200

    added an additional exception handler method

--- src/main/java/com/nix/lpr/library/exception/handler/CustomGlobalExceptionHandler.java
+++ src/main/java/com/nix/lpr/library/exception/handler/CustomGlobalExceptionHandler.java
@@ -8,10 +8,12 @@ import java.time.Instant;
 import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import javax.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 
 @ControllerAdvice
@@ -24,6 +26,12 @@ public class CustomGlobalExceptionHandler {
                 new CustomRestApiException(e.getMessage(), HttpStatus.NOT_FOUND,
                         simpleDateFormat.format(Date.from(Instant.now())));
         return new ResponseEntity<>(restApiException, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleConstraintViolationException(ConstraintViolationException e) {
+        return new ResponseEntity<>("not valid due to validation error: " + e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(value = {RestViolationException.class})
